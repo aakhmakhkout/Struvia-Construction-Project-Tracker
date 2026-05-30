71ff845ab0bcd53df661e217a56d7e00a1cbd8ea
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-05-30 21:17:57 +0530

    Struvia Project Up: Made Landing page responsive

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -13,46 +13,46 @@ const LandingPage = () => {
   const landingPageCards =[
     {
       id: 1,
-      logo: <FileUser size={30} strokeWidth={1.5} />,
+      logo: <FileUser size={20} strokeWidth={1.5} />,
       img: client,
       role: "Client",
       description: "Monitor project progress, track budgets and stay updated."
     },
     {
       id: 2,
-      logo: <Construction size={30} strokeWidth={1.5} />,
+      logo: <Construction size={20} strokeWidth={1.5} />,
       img: contractor,
       role: "Contractor",
       description: "Manage tasks, workers, budgets and ensure smooth project delivery."
     },
     {
       id: 3,
-      logo: <Pickaxe size={30} strokeWidth={1.5} />,
+      logo: <Pickaxe size={20} strokeWidth={1.5} />,
       img: worker,
       role: "Worker",
       description: "Submit work updates, upload site photos and stay on track"
     }
   ]
     return (
-    <div className='w-full min-h-screen relative bg-[url("./src/assets/Bg.jpg")] bg-cover bg-center'>
+    <div className='w-full min-h-screen relative bg-[url("./src/assets/Bg.jpg")] bg-cover bg-center max-md:bg-'>
       <div className='absolute inset-0 bg-black/80'></div>
-      <div className='relative z-10 w-full min-h-screen h-screen flex flex-col  text-white items-center'>
+      <div className='relative z-10 w-full md:h-screen max-md:min-h-screen  flex flex-col  text-white items-center'>
         <div className='flex flex-col h-full w-[90%] justify-around'>
 
         <div className='w-full min-h-60 flex gap-5 items-center'>
-          <div className='w-60 h-50'>
+          <div className='w-60 max-md:w-30 max-sm:w-20'>
             <img src="./src/assets/logos.png" alt="logo"/>
           </div>
           <div>
-            <h1 className='text-8xl font-bold'>STRUVIA</h1>
-            <h3 className='text-3xl text-[#fdba03]'>BUILDING BETTER, TOGETHER</h3>
+            <h1 className='text-8xl font-bold max-md:text-5xl max-sm:text-4xl'>STRUVIA</h1>
+            <h3 className='text-3xl text-[#fdba03] max-md:text-base max-sm:text-sm'>BUILDING BETTER, TOGETHER</h3>
           </div>
         </div>
         
         <div className='flex flex-col gap-5 '>
-          <h1 className='text-5xl font-extralight'>Welcome to <span className='font-bold text-[#fdba03]'>Struvia</span></h1>
+          <h1 className='text-5xl font-extralight max-emd:text-3xl'>Welcome to <span className='font-bold text-[#fdba03]'>Struvia</span></h1>
           <span className='w-30 h-[1.5px] bg-[#fdd703]'></span>
-          <p className='w-[40%] text-white/80'>Your all-in-one platform to track construction projects,
+          <p className='w-[40%] text-white/80 max-lg:w-[60%] max-md:w-[80%] max-sm:w-full'>Your all-in-one platform to track construction projects,
             manage tasks, monitor progress and build with confidence.</p>
         </div>
         
@@ -60,7 +60,7 @@ const LandingPage = () => {
           <LandingPageCards data ={landingPageCards}/>
         </div>
         
-        <div className='flex justify-between'>
+        <div className='flex justify-between max-emd:flex-col max-emd:gap-3'>
           <div>
             <h1 className='flex gap-1 items-center text-sm text-white/80'><Copyright size={18} strokeWidth={1.5} /> <h1>2026 <span className='text-[#fdba03] font-bold'>Noumaan Nabi</span>. All Rights Reserved</h1> </h1>
           </div>
--- src/components/landingPage/LandingPageCards.jsx
+++ src/components/landingPage/LandingPageCards.jsx
@@ -4,27 +4,27 @@ import {ArrowRight} from "lucide-react"
 const LandingPageCards = ({data}) => {
     console.log(data)
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 max-md:my-10">
         <div className="text-2xl font-bold text-[#fdba03]">
             <p>Choose Your Role</p>
         </div>
-    <div className='flex gap-10'>
+    <div className='flex gap-10 max-md:flex-col'>
         {
             data.map((items)=> {
-                return <div key={items.id} className={`flex flex-col bg-white/20 w-80 relative rounded-xl overflow-hidden border border-[#ffa6007e] landingPageCards`}>
+                return <div key={items.id} className={`flex flex-col bg-white/20 w-80 relative rounded-xl overflow-hidden border border-[#ffa6007e] landingPageCards max-emd:w-65`}>
                     <div className=''>
                         <img src={items.img} alt="roleimgs" />
                     </div>
                     <div className='absolute inset-0 bg-black/60 backdrop-blur-[3px]'></div>
 
-                    <div className='absolute z-10 w-full h-full flex flex-col justify-between'>
-                    <div className={`p-3 text-[#fdba03]`} >{items.logo}</div>
-                    <div className="flex flex-col">
-                    <div className=" flex flex-col gap-3 px-2">
-                        <h1 className="text-2xl font-bold">{items.role}</h1>
-                        <p className="text-white/80">{items.description}</p>
+                    <div className='absolute z-10 w-full h-full flex flex-col justify-between '>
+                    <div className={`p-3 text-[#fdba03] max-lg:hidden max-md:block`} >{items.logo}</div>
+                    <div className="flex flex-col max-lg:justify-evenly max-lg:h-full">
+                    <div className=" flex flex-col gap-3 px-2 ">
+                        <h1 className="text-2xl font-bold max-xl:text-xl max-lg:text-lg">{items.role}</h1>
+                        <p className="text-white/80 max-lg:text-sm">{items.description}</p>
                     </div>
-                    <div className=" text-[#fdba03] flex justify-end p-3"><button><ArrowRight size={30} strokeWidth={1.5} /></button></div>
+                    <div className=" text-[#fdba03] flex justify-end p-3 max-lg:p-1"><button><ArrowRight size={30} strokeWidth={1.5} /></button></div>
                     </div>
                     </div>
                 </div>
