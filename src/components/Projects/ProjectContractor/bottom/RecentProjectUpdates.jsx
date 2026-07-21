f75994f7c2940ea18f4953ed77916ae9adb8e86c
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-07-21 12:43:39 +0530

    Struvia Update: add load more button, also some ui changes

--- src/components/Projects/ProjectContractor/bottom/RecentProjectUpdates.jsx
+++ src/components/Projects/ProjectContractor/bottom/RecentProjectUpdates.jsx
@@ -8,8 +8,8 @@ const RecentProjectUpdates = () => {
   console.log(updatesData);
   const filteredData = [...updatesData].reverse();
   return (
-    <div className="CPCards p-3 w-[50%] flex flex-col gap-5 border border-black/20">
-      <div className="flex justify-between">
+    <div className="CPCards p-3 w-[50%] min-h-50 max-h-90 flex flex-col gap-5 border RPUD border-black/20 overflow-y-scroll">
+      <div className="flex justify-between p-2">
         <h1 className="font-bold text-xl">Recent Project Updates</h1>
         <button className="text-[#e55707] text-sm">View All Updates</button>
       </div>
@@ -26,16 +26,16 @@ const RecentProjectUpdates = () => {
                 className="flex justify-between border-t border-black/10 py-3"
               >
                 <div className="flex gap-5 items-center">
-                  <div className="bg-black/20 w-15 h-10 flex items-center justify-center rounded-sm overflow-hidden">
+                  <div className=" w-15 h-10 flex items-center justify-center rounded-sm overflow-hidden">
                     {items.type !== "tasks" ? (
                       <img
-                        className="w-full h-full"
+                        className="w-full h-full bg-black/20"
                         src={items.coverImg.src}
                         alt={items.coverImg.orginalName}
                       />
                     ) : (
-                      <div>
-                        <ClipboardList size={20} strokeWidth={1.25} />
+                      <div className="bg-[#9895dc4c] w-full h-full flex justify-center items-center">
+                        <ClipboardList size={30} strokeWidth={1.25} />
                       </div>
                     )}
                   </div>
@@ -67,6 +67,9 @@ const RecentProjectUpdates = () => {
           </button>
         </div>
       )}
+      <button className="flex w-full justify-center items-center py-2 rounded-lg bg-black/10 border border-black/10 cursor-pointer">
+        Load More
+      </button>
     </div>
   );
 };
